cc2920c9634515c5d98456b2df96d5bd698f3b9f
Ariel Jurkevitch <arieljurkevitch@gmail.com>
2022-08-19 15:30:00 +0200

    Rails watch list

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,41 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[show destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.valid?
+      @list.save
+      redirect_to list_path(@list)
+    # No need for app/views/restaurants/create.html.erb
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
